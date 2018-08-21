979386fd2a0d7ea640b43b2e8992c36f7c5ab901
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-08-21 15:01:10 +0100

    WIP - Removed ansi ctl output

--- acceptance-tests/run-regression.sh
+++ acceptance-tests/run-regression.sh
@@ -2,8 +2,8 @@
 tearDown() {
   echo "Tearing down existing dev-env-develop directory"
   cd dev-env-develop
-  docker-compose kill
-  docker-compose rm -f
+  docker-compose kill --no-ansi
+  docker-compose rm -f --no-ansi
   cd ..
   rm -rf dev-env-develop
 }
@@ -46,8 +46,8 @@ gradle :outputComputedVersion
 
 cd dev-env-develop
 bash load-modules.sh
-docker-compose build 
-docker-compose up -d
+docker-compose build --no-ansi
+docker-compose up -d --no-ansi
 ./wait_for_it.sh localhost:5432 localhost:8681:/manage/actuator/health localhost:8381:/manage/actuator/health localhost:8281:/manage/actuator/health localhost:8081:/manage/actuator/health localhost:8481:/manage/actuator/health localhost:8181:/manage/actuator/health localhost:8581:/manage/actuator/health
 psql -h localhost -U developer -d bb_dev -f ./scripts/db/setup-user.sql 
 
