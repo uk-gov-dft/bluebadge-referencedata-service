c6747b4db3374bd28350638cc5c5410fb3a9f5de
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-11-08 13:09:10 +0000

    BBB-1015 - Moved BULKEQUIP - NONE to OTHER

---
+++ database-schema/scripts/20181108000000_BBB-1015-add-othermedicalequipment.sql
@@ -0,0 +1,8 @@
+UPDATE reference_data SET code = 'OTHER', description = 'Other'
+WHERE data_group_id = 'BULKEQUIP' and code = 'NONE';
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+UPDATE reference_data SET code = 'OTHER', description = 'Other'
+WHERE data_group_id = 'BULKEQUIP' and code = 'NONE';
+
