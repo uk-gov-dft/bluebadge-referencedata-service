a51a4fb6bb15438cd92b692cdcf0a1b70852d5de
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-11-08 13:49:17 +0000

    BBB-1015 - Fix teardown script

--- database-schema/scripts/20181108000000_BBB-1015-add-othermedicalequipment.sql
+++ database-schema/scripts/20181108000000_BBB-1015-add-othermedicalequipment.sql
@@ -3,6 +3,6 @@ WHERE data_group_id = 'BULKEQUIP' and code = 'NONE';
 
 -- //@UNDO
 -- SQL to undo the change goes here.
-UPDATE reference_data SET code = 'OTHER', description = 'None'
-WHERE data_group_id = 'BULKEQUIP' and code = 'NONE';
+UPDATE reference_data SET code = 'NONE', description = 'None'
+WHERE data_group_id = 'BULKEQUIP' and code = 'OTHER';
 
