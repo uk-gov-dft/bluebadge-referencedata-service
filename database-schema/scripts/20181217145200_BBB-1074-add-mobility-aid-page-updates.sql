215673c0d04940483e996359c56cb3c7d0b16112
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-12-18 11:11:52 +0000

    BBB-1074 - Fixed migrate script

--- database-schema/scripts/20181217145200_BBB-1074-add-mobility-aid-page-updates.sql
+++ database-schema/scripts/20181217145200_BBB-1074-add-mobility-aid-page-updates.sql
@@ -1,3 +1,2 @@
 INSERT INTO reference_data (data_group_id, code, description, display_order, data_subgroup_id) VALUES
-('WALKMOB', 'PERSON', 'It\\'s a person, rather than a mobility aid', 4, null)
-;
+('WALKMOB', 'PERSON', 'It''s a person, rather than a mobility aid', 4, null);
