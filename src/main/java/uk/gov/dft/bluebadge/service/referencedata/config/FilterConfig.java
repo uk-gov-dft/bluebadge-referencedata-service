cf0b80bad3c27435e91ab086cc9d69b63baa5648
Robert Smallwood <38761367+robsvaltech@users.noreply.github.com>
2019-02-25 14:22:17 +0000

    BBB-1127 - Added JwtMdcFilter (#41)
    * BBB-1127 - Added JwtMdcFilter
    
    * BBB-1127 - Added LoggingAspect

--- src/main/java/uk/gov/dft/bluebadge/service/referencedata/config/FilterConfig.java
+++ src/main/java/uk/gov/dft/bluebadge/service/referencedata/config/FilterConfig.java
@@ -3,12 +3,18 @@ package uk.gov.dft.bluebadge.service.referencedata.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.dft.bluebadge.common.esapi.EsapiFilter;
+import uk.gov.dft.bluebadge.common.logging.JwtMdcFilter;
 
 @Configuration
-public class EsapiConfig {
+public class FilterConfig {
 
   @Bean
   public EsapiFilter getEsapiFilter() {
     return new EsapiFilter();
+  }
+
+  @Bean
+  public JwtMdcFilter getJwtMdcFilter() {
+    return new JwtMdcFilter();
   }
 }
