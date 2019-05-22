d126db8142b2278cc9a4d3d38167e4933c4fde30
Paul Roberts <paul.roberts@valtech.com>
2019-05-22 16:20:38 +0100

    BBB-1419 reorder cancel reasons

--- database-schema/scripts/20190521111118_BBB-1419-new-cancellation-and-replace-reasons.sql
+++ database-schema/scripts/20190521111118_BBB-1419-new-cancellation-and-replace-reasons.sql
@@ -5,11 +5,15 @@ INSERT INTO referencedata.reference_data
  (data_group_id,code,description,display_order)
 VALUES
  ('REPLACE','UNDELIVER','Undelivered - Lost in post',4)
-,('CANCEL' ,'UNDELIVER','Undelivered - Lost in post',4)
-,('CANCEL' ,'LOST'     ,'Lost',5)
+,('CANCEL' ,'UNDELIVER','Undelivered - Lost in post',7)
+,('CANCEL' ,'LOST'     ,'Lost',3)
 ,('CANCEL' ,'STOLE'   ,'Stolen',6)
-,('CANCEL' ,'DAMAGED'  ,'Damaged',7)
+,('CANCEL' ,'DAMAGED'  ,'Damaged',1)
 ;
+
+UPDATE referencedata.reference_data set display_order = 2 WHERE data_group_id = 'CANCEL' AND code = 'DECEASE';
+UPDATE referencedata.reference_data set display_order = 4 WHERE data_group_id = 'CANCEL' AND code = 'NOLONG';
+UPDATE referencedata.reference_data set display_order = 5 WHERE data_group_id = 'CANCEL' AND code = 'REVOKE';
 
 
 -- //@UNDO
